686d961a22943588870ad11064dfcbb8a73c1e75
coolavy <avyangupta@gmail.com>
2025-12-07 17:18:32 -0500

    fsdafdsad

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
 
   // REQUIRED for GitHub Pages!
   base: "/RenaissanceHistoryHub/",

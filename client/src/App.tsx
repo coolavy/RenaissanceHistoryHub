a542478f92005cf5d9a34cf4531d53f7e886a7e3
coolavy <avyangupta@gmail.com>
2025-12-07 17:42:48 -0500

    affdas

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -14,7 +14,9 @@ import WhyItFits from "@/pages/why-it-fits";
 import Bibliography from "@/pages/bibliography";
 import NotFound from "@/pages/not-found";
 
-function Router() {
+const BASE_PATH = "/RenaissanceHistoryHub";
+
+function AppRouter() {
   return (
     <Switch>
       <Route path="/" component={Home} />
@@ -30,20 +32,22 @@ function Router() {
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider defaultTheme="light">
-        <TooltipProvider>
-          <div className="min-h-screen flex flex-col">
-            <Navigation />
-            <main className="flex-1">
-              <Router />
-            </main>
-            <Footer />
-          </div>
-          <Toaster />
-        </TooltipProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
+    <Router base={BASE_PATH}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider defaultTheme="light">
+          <TooltipProvider>
+            <div className="min-h-screen flex flex-col">
+              <Navigation />
+              <main className="flex-1">
+                <AppRouter />
+              </main>
+              <Footer />
+            </div>
+            <Toaster />
+          </TooltipProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </Router>
   );
 }
 
